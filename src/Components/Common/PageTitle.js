abbfe4a28807cb3a62a4e201cc2397a8a4b59b09
anujsinghwd <anuj.s@mapmyindia.com>
2019-05-30 14:21:34 +0530

    post update

--- src/Components/Common/PageTitle.js
+++ src/Components/Common/PageTitle.js
@@ -5,7 +5,7 @@ class PageTitle extends Component {
     return (
       <section
         className="bg-title-page p-t-50 p-b-40 flex-col-c-m"
-        style={{backgroundImage: "url('images/BANNER.jpg')"}}
+        style={{backgroundImage: "url('images/banner.jpg')"}}
       >
         <h2 className="l-text2 t-center">Products</h2>
         <p className="m-text13 t-center">New Arrivals Collection 2019</p>
